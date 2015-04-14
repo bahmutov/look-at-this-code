901baaa7b3002e45c636c45031bb9c8ae00ed707
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-04-14 18:12:11 -0400

    working code with ace

--- public/js/viewer-app.js
+++ public/js/viewer-app.js
@@ -1,5 +1,51 @@
-$(document).ready(function() {
+//= require lib/ui-ace/ace.min.js
+//= require lib/ui-ace/ui-ace.min.js
 
-  console.log('viewer app');
+(function startViewerApp(angular) {
 
-});
+  var app = angular.module('ViewerApp', ['ui.ace']);
+
+  app.controller('EditorController', function ($scope, $element) {
+    $scope.shownSource = $element.find('pre').html();
+
+    function clearSelection() {
+      $scope.selection = null;
+      $scope.selectedText = null;
+    }
+
+    $scope.aceLoaded = function(_editor) {
+      console.log('loaded editor', _editor);
+      $scope.editor = _editor;
+
+      $scope.editor.getSession().selection.on('changeSelection', function (e) {
+        var selection = $scope.editor.getSelectionRange();
+        if (selection.isEmpty()) {
+          clearSelection();
+        } else {
+          $scope.selection = selection;
+          $scope.selectedText = $scope.editor.session.getTextRange($scope.selection);
+          console.log('selection changed', $scope.selection.toString());
+        }
+        if (!$scope.$$phase) {
+          $scope.$apply();
+        }
+      });
+    };
+
+    $scope.aceChanged = function(e) {
+      // console.log('editor content changed', e);
+    };
+
+    $scope.addReviewNote = function addReviewNote() {
+      if ($scope.selection) {
+        var entered = prompt('Enter review note');
+        if (entered) {
+          clearSelection();
+          $scope.editor.selection.clearSelection();
+        }
+      }
+    };
+  });
+
+  angular.bootstrap(document.body, ['ViewerApp']);
+}(window.angular));
